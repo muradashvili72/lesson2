d4aa6466e31367fb980c0016e81cf004643262f7
muradashvili72 <103437121+muradashvili72@users.noreply.github.com>
2022-05-28 14:03:16 +0400

    add to homework 2

---
+++ script.js
@@ -0,0 +1,77 @@
+// 1. შექმენით ფუნქცია, რომელსაც გადავცემთ n რაოდენობის მნიშვნელობას და დააბრუნოს მხოლოდ დადებითი
+//  რიცხვების ჯამს:2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6, 8;
+
+// function Nnumbers (...n){
+//     let numbersum = 0;
+//     for(let x of n){
+//         if(x>0){
+//             numbersum = numbersum + x;
+//         }
+// }
+// return numbersum;
+// }
+// let n=Nnumbers(2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6, 8);
+// console.log(n)
+
+
+
+
+// 2. ფუნქციის საშულებით გამოიტანეთ ყველა რიცხვის ჯამი:
+// 10, 50, 6, 7, 8, 11, 6, 3, 9
+
+// function array(...N){
+//     let numbers=0;
+    
+//   for (let y of N ){
+//         numbers=numbers+y;
+//     }
+//          return numbers;
+//     }
+    
+//     let x= array(10, 50, 6, 7, 8, 11, 6, 3, 9)
+//     console.log(x);
+    
+
+
+
+
+// 3. let user = {
+//     firstname: 'giorgi',
+//     lastname: 'saakadze',
+//     age: 32,
+//     isloggedin: true
+//   }
+  
+//   ფუნქციამ უნდა დააბრუნოს სახელი და გვარი ადგილის გამოტოვებით (მაგ: 'giorgi saakadze') 
+//   თუ isloggedin არის true და false თუ isloggedin ფროფერთი არის false;
+
+
+// function array (isloggedin) {
+//     if (isloggedin == true) {
+//         console.log(user.firstname, user.lastname);
+//         return user.firstname, user.lastname;
+//     }
+//     else {
+//         console.log('false');
+//         return false;
+//     }
+// }
+// array(user.isloggedin);
+
+
+
+
+// 4. შექმენით ფუნქცია რომელსაც გადაეცემა ერთი პარამეტრი. ეს პარამეტრი უნდა იყოს რიცხვების 
+// მასივი და ფუნქციამ უნდა დააბრუნოს ამ რიცხვებიდან მაქსიმალური:
+
+
+// let array = [37, 22, 8, 15, 72, 10];
+// X = 0;
+
+// array.forEach((elements) => {
+//   if (X < elements) {
+//     X = elements;
+//   }
+// })
+// console.log(X);
+
